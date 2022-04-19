08144de03becac9802cc180106d35219bd7deab3
KeremP <kerpro26@gmail.com>
2022-04-19 11:56:10 -0400

    implement main handler function. check image filetype and convert to png to be rendered on canvas.

---
+++ index.js
@@ -0,0 +1,12 @@
+const  { downloadImage, convert, checkType } = require('./utils');
+
+// checks file type and converts from .ai or .eps to .png
+// using imagemagick. the returned buffer will be rendered on fabric.js canvas
+exports.handler = async (event) => {
+    const buf = await downloadImage(event.url);
+    const fileType = await checkType(buf);
+    if (fileType.ext == '.ai' || fileType.ext == '.eps') {
+        const converted = await convert(buf);
+        return { converted };
+    }
+};

851630de41b509aafbde71829cb258e3e35bfb00
KeremP <kerpro26@gmail.com>
2022-04-20 13:11:10 -0400

    removed GM dependency as it was not behaving well with imagemagick. rewrote convert function to use temp file built from downloadImage buffer. tested and working on Lambda. can consume a lot of memory however, may need to refactor more.

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const  { downloadImage, convert, checkType } = require('./utils');
+const  { downloadImage, writeBufferToTempFile, convertCmd, convertBase64 } = require('./utils');
 
 // checks file type and converts from .ai or .eps to .png
 // using imagemagick. the returned buffer will be rendered on fabric.js canvas
 exports.handler = async (event) => {
     const buf = await downloadImage(event.url);
-    const fileType = await checkType(buf);
-    if (fileType.ext == '.ai' || fileType.ext == '.eps') {
-        const converted = await convert(buf);
-        var base64 = converted.toString('base64');
-        return { base64 };
-    }
+    // const inputType = await checkType(buf);
+    const outputTmp = await writeBufferToTempFile(buf);
+    const outpath = await convertCmd(outputTmp);
+    const output = await convertBase64(outpath);
+    return { output };
 };
--- utils.js
+++ utils.js
@@ -1,22 +1,70 @@
 const FT = require('file-type');
-const GM = require('gm');
-const gm = GM.subClass({ imageMagick: true });
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 const axios = require('axios');
+const Q = require('q');
+const fs = require('fs');
+const tmp = require('tmp');
+const childProcess = require('child_process');
+const util = require('util');
+const exec = util.promisify(childProcess.exec);
+const nanoid = require('nanoid/async');
 
 exports.downloadImage = async (url) => {
     const res = await axios.get(url, {responseType: 'arraybuffer'});
-    return Buffer.from(res.data, 'binary');
+    return Buffer.from(res.data,'binary');
 };
 
-exports.convert = async (buf) => {
-    return new Promise(( resolve, reject ) => {
-        gm(buf).toBuffer('PNG', function(err, buffer) {
-            err ? reject(err) : resolve(buffer);
-        });
+// TODO: fix tmp file writing, or put object to s3 bucket temporarily
+//       then delete from temp bucket dir on complete.
+//       OR: write to temp_out dir -> need to test this.
+
+function writeBufferToFile(fd, buffer) {
+    var deffered = Q.defer();
+    fs.write(fd, buffer, 0, buffer.length, 0, function(err, written, buffer) {
+        if(!written) {
+            deffered.reject(err);
+        } else {
+            deffered.resolve(true);
+        }
     });
+    return deffered.promise;
+}
+
+// write buffer to tmp file, then execute magick cmd
+exports.writeBufferToTempFile = function(buffer) {
+    var deffered = Q.defer();
+    tmp.file(function _tempFileCreated(err, path, fd, cleanupCallback) {
+        if (err) {
+            deffered.reject(err);
+        } else {
+            writeBufferToFile(fd, buffer).then(
+                function(isWritten) { deffered.fulfill(path); },
+                function(err) {deffered.reject(err);}
+            );
+        }
+    });
+    return deffered.promise;
 };
+
+exports.convertCmd = async (fp) => {
+    const id = await nanoid.nanoid();
+    const out = '/tmp/'+id+'.png';
+    try{
+        const { stdout } = await exec(
+            `magick ${fp} ${out}`
+        );
+        return out;
+    } catch(e) {
+        throw new Error('Error in convert cmd: '+ e.message);
+    }
+}
+
+exports.convertBase64 = async (fp) => {
+    const b64 = fs.readFileSync(fp, {encoding:'base64'});
+    fs.unlinkSync(fp);
+    return b64;
+}
 
 exports.checkType = async (buf) => {
     const contentType = await FT.fromBuffer(buf);
